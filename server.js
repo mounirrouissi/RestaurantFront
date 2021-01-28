706acadb7cee8dbbd45ae49e593324bca00daff4
mounirrouissi <mounirrouissi2@gmail.com>
2021-01-28 22:44:54 +0100

    heroku test 1

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const express=require('express');
-const path=require('path');
-const app=express();
-app.use(express.static('./dist/angular1'));
-app.get('/*',function(req,res){
-  res.sendFile(path.join('./dist/angular1/index.html'));});
-    app.listen(process.env.PORT || 8081);
 
+    const express = require('express');
+
+    const app = express();
+
+    app.use(express.static('./dist/angular1'));
+
+    app.get('/*', (req, res) =>
+        res.sendFile('index.html', {root: 'dist/angular1/'}),
+    );
+
+    app.listen(process.env.PORT || 8080);
